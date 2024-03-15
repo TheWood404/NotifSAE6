ca9d5596dcac0b3e246946bfa2607318f8c806f8
Jules <jules.gouerec@gmail.com>
2024-03-15 08:23:43 +0100

    ajout reset notif.json

--- appnotif/script.js
+++ appnotif/script.js
@@ -1,12 +1,8 @@
-// Fonction pour récupérer les notifications du serveur
 function getNotification() {
-    // Appel à l'API pour récupérer les notifications
     fetch('http://localhost:3000/read-notification')
     .then(response => response.json())
     .then(data => {
-        // Vérifie si une notification doit être affichée
         if (data.message) {
-            // Affiche la notification
             const notificationDiv = document.getElementById('notification');
             notificationDiv.style.display = 'block';
             notificationDiv.innerHTML = `<strong>Notification:</strong> ${data.message}`;
@@ -17,5 +13,4 @@ function getNotification() {
     });
 }
 
-// Vérification des notifications toutes les 5 secondes
 setInterval(getNotification, 5000);
--- servnotif/server.js
+++ servnotif/server.js
@@ -1,29 +1,29 @@
 const express = require('express');
-const cors = require('cors'); // Importez le module cors
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 const fs = require('fs');
 const path = require('path');
 
-// Activez CORS
 app.use(cors());
 
+const notificationFilePath = path.join(__dirname, 'notification.json');
 
-// Point de terminaison API pour vérifier si une notification doit être envoyée
+if (fs.existsSync(notificationFilePath)) {
+    fs.unlinkSync(notificationFilePath);
+}
+
+fs.writeFileSync(notificationFilePath, JSON.stringify({}));
+
 app.get('/send-notification', (req, res) => {
-    // Récupérer les paramètres de la requête
     const message = req.query.message;
 
-    // Construire l'objet JSON avec la notification
     const notificationData = { message: message };
 
-    // Convertir l'objet JSON en chaîne JSON
     const notificationJSON = JSON.stringify(notificationData);
 
-    // Chemin du fichier de notification JSON
     const notificationFilePath = path.join(__dirname, 'notification.json');
 
-    // Écrire la notification JSON dans le fichier
     fs.writeFile(notificationFilePath, notificationJSON, (err) => {
         if (err) {
             console.error('Erreur lors de l\'écriture de la notification dans le fichier:', err);
@@ -35,18 +35,16 @@ app.get('/send-notification', (req, res) => {
     });
 });
 
-// Point de terminaison API pour lire la notification depuis le fichier temporaire
 app.get('/read-notification', (req, res) => {
     const notificationFilePath = path.join(__dirname, 'notification.json');
 
-    // Lire la notification à partir du fichier temporaire
     fs.readFile(notificationFilePath, 'utf8', (err, data) => {
         if (err) {
             console.error('Erreur lors de la lecture de la notification depuis le fichier:', err);
             res.status(500).send('Erreur lors de la lecture de la notification depuis le fichier');
         } else {
             console.log('Notification lue depuis le fichier:', data);
-            res.status(200).send(data); // Envoyer le contenu du fichier comme réponse
+            res.status(200).send(data);
         }
     });
 });
